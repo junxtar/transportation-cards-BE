edbbe34cc21613a65edbb56da55d5d2f82bcaebe
junxtar <aa1870088@mju.ac.kr>
2025-03-10 14:01:21 +0900

    test: 테스트 코드 작성

---
+++ src/test/java/com/junxtar/transportationcardsbe/service/TransportationCardServiceTest.java
@@ -0,0 +1,50 @@
+package com.junxtar.transportationcardsbe.service;
+
+import com.junxtar.transportationcardsbe.domain.CommuteType;
+import com.junxtar.transportationcardsbe.domain.UserCategory;
+import com.junxtar.transportationcardsbe.dto.request.RecommendTransportationCardRequestDto;
+import com.junxtar.transportationcardsbe.dto.response.RecommendTransportationCardResponseDto;
+import com.junxtar.transportationcardsbe.repository.TransportationCardRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+class TransportationCardServiceTest {
+
+    @InjectMocks
+    private TransportationCardService transportationCardService;
+
+    @Mock
+    private TransportationCardRepository transportationCardRepository;
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+    @Test
+    @DisplayName("저소득층_따릉이사용_출근_서울지하철_퇴근_서울지하철_비용을계산한다")
+    void 저소득층_따릉이사용_출근_서울지하철_퇴근_서울지하철_비용을계산한다() {
+        // given
+        RecommendTransportationCardRequestDto requestDto = RecommendTransportationCardRequestDto.builder()
+            .userCategory(UserCategory.LOW_INCOME)
+            .isSeoulBike(true)
+            .commuteType(CommuteType.SEOUL_SUBWAY)
+            .returnCommuteType(CommuteType.SEOUL_SUBWAY)
+            .build();
+
+        // when
+        RecommendTransportationCardResponseDto recommendTransportationCardResponseDto = transportationCardService.recommendTransportationCard(
+            requestDto);
+
+        // then
+        Long expectedKPassPrice = 33623L;
+        Long expectedClimateActionPrice = 48000L;
+
+        Assertions.assertEquals(expectedKPassPrice, recommendTransportationCardResponseDto.getKPassPrice());
+        Assertions.assertEquals(expectedClimateActionPrice, recommendTransportationCardResponseDto.getClimateActionPrice());
+     }
+
+}
