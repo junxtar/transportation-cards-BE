12a9fb6a311ff720dbae9ea0b76d888ce3d2c3e3
junxtar <aa1870088@mju.ac.kr>
2025-03-10 10:49:06 +0900

    first commit

---
+++ src/main/java/com/junxtar/transportationcardsbe/TransportationCardsBeApplication.java
@@ -0,0 +1,13 @@
+package com.junxtar.transportationcardsbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportationCardsBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransportationCardsBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/junxtar/transportationcardsbe/TransportationCardsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junxtar.transportationcardsbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportationCardsBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
